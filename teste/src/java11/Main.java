53c7a29ee77d675d00ba38c434dccf4ae6bca3ca
Joel Lobo <joellobo@gmail.com>
2021-04-01 22:59:54 -0300

    Added stuff

--- teste/src/java11/Main.java
+++ teste/src/java11/Main.java
@@ -1,6 +1,5 @@
 package java11;
 
-import java.util.List;
 import java.util.stream.Stream;
 
 public class Main {
@@ -10,15 +9,9 @@ public class Main {
 		String fullName = "Joel Lobo de Brito Neto";
 		Stream<String> lines = fullName.lines();
 		
-		System.out.println(lines);
-		
-//		
-//		List<String> ordensIDs =    
-//				lines
-//	               //.filter(o -> o.getType() == Ordem.ATIVACAO)
-//	               //.sorted(comparing(Ordem::getValue).reversed())
-//	               .map()
-//	               .collect(System.out::println);
+		lines
+		.map(e -> e + ",")
+		.forEach(System.out::print);
 
 	}
 
